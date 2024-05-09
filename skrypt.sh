f013b9dfbc7c9b6d395140537a303ab5c3a0ec25
Jaro <pompabicki69@gmail.com>
2024-05-09 15:03:34 +0200

    wow

--- skrypt.sh
+++ skrypt.sh
@@ -1,2 +1,23 @@
 #!/bin/bash
-date
+if [ "$1" == "--date" ]; then
+    date
+elif [ "$1" == "--logs" ]; then
+    if [ -z "$2" ]; then
+        num_logs=100
+    else
+        num_logs=$2
+    fi
+    for ((i=1; i<=$num_logs; i++)); do
+        filename="log$i.txt"
+        echo "Nazwa pliku: $filename" > "$filename"
+        echo "Nazwa skryptu: $0" >> "$filename"
+        echo "Data: $(date)" >> "$filename"
+    done
+elif [ "$1" == "--help" ]; then
+    echo "Dostępne opcje:"
+    echo "- skrypt.sh --date : Wyświetla dzisiejszą datę"
+    echo "- skrypt.sh --logs [liczba] : Tworzy automatycznie podaną liczbę plików log.txt"
+    echo "- skrypt.sh --help : Wyświetla dostępne opcje"
+else
+    echo "Nierozpoznana opcja. Użyj 'skrypt.sh --help' aby wyświetlić dostępne opcje."
+fi
